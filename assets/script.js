bd07798798ce76993ee32a77b31df505d3efe564
David Codner <dccodner24@gmail.com>
2022-10-04 20:01:42 -0500

    Major overhaul to quiz functions.

--- assets/script.js
+++ assets/script.js
@@ -9,60 +9,15 @@ var ansOne = $('#0');
 var ansTwo = $('#1');
 var ansThree = $('#2');
 var ansFour = $('#3');
-
 var recordMenu = $('.results');
 var ans = $('.answer')
 var questionBox = $('#question');
-
-
-//Starting conditionds
+var ansStatus = $('.question-response')
 var secondsLeft = 0;
-var timer;
+var x = 0;
+var secDelay = 0;
+var submitBtn = $('#submit')
 
-
-
-function startQuiz(){
-    // Starts timer and presents user with questions and answers
-    secondsLeft= 100;
-    timer = setInterval(clock, 1000)
-    startMenu.css('display', 'none');
-    quizMenu.css('display','flex');
-    timerEl.text(secondsLeft);
-    ans.on('click',ansQuestion);
-
-    // Assigns initial values to question and answer fields
-    questionBox.text(questions[0].question);
-    ansOne.text(questions[0].answers[0]);
-    ansTwo.text(questions[0].answers[1]);
-    ansThree.text(questions[0].answers[2]);
-    ansFour.text(questions[0].answers[3]);  
-}
-
-function ansQuestion(clicked_question){
-    console.log(clicked_question);
-}
-
-
-function clock(){
-    secondsLeft--;
-    timerEl.text(secondsLeft);
-    if (secondsLeft <= 0){
-        endQuiz();
-
-    }
-}
-
-function endQuiz(){
-    clearInterval(timer);
-    quizMenu.css('display', 'none')
-    recordMenu.css('display','flex');
-}
-
-startBtn.on('click',startQuiz);
-
-
-
-//Questions coded as objects inside of an array for organization.
 
 var questions = [
     {
@@ -101,3 +56,89 @@ var questions = [
         correct: 'Alert'
     }
 ]
+
+
+function clock(){
+    secondsLeft--;
+    timerEl.text(secondsLeft);
+    if (secondsLeft <= 0){
+        endQuiz();
+
+    }
+}
+
+
+function startQuiz(){
+    // Starts timer and presents user with questions and answers
+    secondsLeft= 100;
+    x = 0
+    timer = setInterval(clock, 1000)
+    startMenu.css('display', 'none');
+    quizMenu.css('display','flex');
+    timerEl.text(secondsLeft);
+    ans.on('click',ansQuestion);
+
+    // Assigns initial values to question and answer fields
+    questionBox.text(questions[x].question);
+    ansOne.text(questions[x].answers[0]);
+    ansTwo.text(questions[x].answers[1]);
+    ansThree.text(questions[x].answers[2]);
+    ansFour.text(questions[x].answers[3]);  
+}
+
+function endQuiz(){
+    clearInterval(timer);
+    x = 0;
+    quizMenu.css('display', 'none')
+    recordMenu.css('display','flex');
+}
+
+function ansQuestion(index){
+   
+    var response = ''
+    var response = questions[x].answers[index];
+    // console.log(x);
+    
+    if(response === questions[x].correct){
+        ansStatus.text('Correct');
+        ansStatus.css('display','flex');
+        setTimeout(() => { ansStatus.css('display','none'); }, 2000);
+        x++;
+        indexChk();
+
+    }
+
+    else{
+        secondsLeft = secondsLeft - 10;
+        ansStatus.text('Incorrect');
+        ansStatus.css('display','flex');
+        setTimeout(() => { ansStatus.css('display','none'); }, 2000);
+        x++;    
+        indexChk();    
+    }
+}
+
+// function storeScore(){
+
+// }
+
+function indexChk(){
+    if(x > questions.length){
+        endQuiz();
+    }
+
+    else{
+        questionBox.text(questions[x].question);
+        ansOne.text(questions[x].answers[0]);
+        ansTwo.text(questions[x].answers[1]);
+        ansThree.text(questions[x].answers[2]);
+        ansFour.text(questions[x].answers[3]);
+    }
+}
+
+startBtn.on('click',startQuiz);
+
+// submitBtn.on('click',storeScore);
+  
+
+
